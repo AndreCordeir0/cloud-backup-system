aa1fcb582a06563b3d6718ad05859cd6a7a7a049
Andre Cordeiro <andrezaocordeiro@gmail.com>
2023-12-21 21:04:11 -0300

    feat: criando userUpload entity

---
+++ src/main/java/com/cloud/backup/system/dao/impl/UserUploadsDAO.java
@@ -0,0 +1,6 @@
+package com.cloud.backup.system.dao.impl;
+
+import com.cloud.backup.system.model.impl.UserUploads;
+
+public class UserUploadsDAO extends GenericDaoImpl<UserUploads> {
+}
---
+++ src/main/java/com/cloud/backup/system/model/impl/UserUploads.java
@@ -0,0 +1,54 @@
+package com.cloud.backup.system.model.impl;
+
+
+import com.cloud.backup.system.model.Model;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(schema = "cloud", name = "TB_USER_UPLOADS")
+public class UserUploads implements Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", updatable = false, nullable = false)
+    private User user;
+
+    @Column(name = "is_unsafe")
+    private Boolean isUnsafe = false;
+
+    @Column(name = "uuid", nullable = false, updatable = false)
+    private UUID uuid;
+
+    @Column
+    private String name;
+
+    @Column(updatable = false)
+    private LocalDateTime uploadDate;
+
+    @Column
+    private LocalDateTime removedDate;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Boolean getUnsafe() {
+        return isUnsafe;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+}
