07a60d9e294f40c7c7c0964e93a0af898bc7d63e
Andre Cordeiro <andrezaocordeiro@gmail.com>
2023-12-27 02:11:39 -0300

    feat: criando endpoint para o envio de arquivos

---
+++ src/main/java/com/cloud/backup/system/model/impl/FormData.java
@@ -0,0 +1,34 @@
+package com.cloud.backup.system.model.impl;
+
+import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.core.MediaType;
+import org.apache.tika.Tika;
+import org.jboss.resteasy.annotations.providers.multipart.PartType;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serial;
+import java.io.Serializable;
+
+public class FormData implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 6354625779791548994L;
+
+    @FormParam("file")
+    @PartType(MediaType.APPLICATION_OCTET_STREAM)
+    public byte[] file;
+
+    @FormParam("name-file")
+    @PartType(MediaType.TEXT_PLAIN)
+    public String fileName;
+
+    @FormParam("folder")
+    @PartType(MediaType.TEXT_PLAIN)
+    public String folder;
+
+    // Add a method to get the MIME type using Apache Tika
+    public String getMimeType() {
+        return new Tika().detect(file);
+    }
+}
--- src/main/java/com/cloud/backup/system/rest/UserUploadsRest.java
+++ src/main/java/com/cloud/backup/system/rest/UserUploadsRest.java
@@ -1,15 +1,18 @@
 package com.cloud.backup.system.rest;
 
+import com.cloud.backup.system.model.impl.FormData;
 import com.cloud.backup.system.security.ClaimEnum;
 import com.cloud.backup.system.service.impl.UserUploadsService;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
 
 @Path("/upload")
 public class UserUploadsRest {
@@ -28,5 +31,13 @@ public class UserUploadsRest {
         return Response.ok(
                 userUploadsService.createFolder(folderName, ctx.getClaim(ClaimEnum.ID.getClaim()))
         ).build();
+    }
+
+    @POST
+    @Path("/stream")
+    @RolesAllowed({"USER", "ADMIN"})
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response saveStream(@MultipartForm FormData formData) {
+        return Response.ok(userUploadsService.saveStream(formData)).build();
     }
 }
