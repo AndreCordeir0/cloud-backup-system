0d3ee21e7ac2e781d30c7f447d0664b190ca213f
Andre Cordeiro <andrezaocordeiro@gmail.com>
2023-12-28 21:35:46 -0300

    feat: creating unsafe endpoint

--- src/main/java/com/cloud/backup/system/rest/UserUploadsRest.java
+++ src/main/java/com/cloud/backup/system/rest/UserUploadsRest.java
@@ -6,10 +6,7 @@ import com.cloud.backup.system.service.impl.UserUploadsService;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.validation.Valid;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.jwt.JsonWebToken;
@@ -39,6 +36,16 @@ public class UserUploadsRest {
     @RolesAllowed({"USER", "ADMIN"})
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     public Response saveStream(@MultipartForm @Valid FormData formData) {
-        return Response.ok(userUploadsService.saveStream(formData, ctx.getClaim(ClaimEnum.ID.getClaim()))).build();
+        return Response.ok(userUploadsService.saveFile(formData, ctx.getClaim(ClaimEnum.ID.getClaim()))).build();
+    }
+
+
+
+    @PUT
+    @Path("/unsafe/{id}")
+    @RolesAllowed({"ADMIN"})
+    public Response markUnsafe(@PathParam("id") Long id) {
+        userUploadsService.markUnsafe(id);
+        return Response.accepted().build();
     }
 }
